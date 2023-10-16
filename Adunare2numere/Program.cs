9b2dce88621452a92634b093b56777a1bf6b5a13
SabdruEzechiel <ezechiel.sandru@emanuel.ro>
2023-10-16 22:24:42 +0300

    a

---
+++ Adunare2numere/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Adunare2numere
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            a=int.Parse(Console.ReadLine());
+            b=int.Parse(Console.ReadLine());
+            Console.WriteLine(a + b);
+        }
+    }
+}
